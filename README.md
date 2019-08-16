431fcb344ae54f1d6fa59bb7429928d1671bd427
Damon Macdonald <macdonalddm@familysearch.org>
2019-08-16 15:17:24 -0600

    sample python training code and vectors

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # learn-xgboost
 Binary classification example code and data for xgboost
 
-## data
-There are 2702 pairs  in csv format in the data directory (basically deval) 1193 neg (start with "0"), and 1467 pos (start with "1").  The fields then alternate after the label for target, then candidate.  They include person name, spouse, parent1, parent2, child, person birth year, month, day, country, county, city, location, census year, month, day, country, county, city, location, and death year, month, day, country, county, city, location.  The last field is a reference id.
+Experimenting with scikit
+================
+
+[Scikit-learn](http://scikit-learn.org/stable/) supports many different classification methods, with good documentation and example code in python.  You might want to look at the [sklearn.svm.SVC docs](https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html), and the excellent [user guide](https://scikit-learn.org/stable/modules/svm.html#svm-classification).  The [kernel functions](https://scikit-learn.org/stable/modules/svm.html#svm-kernels), and the [RBF SVM parameters](https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html#sphx-glr-auto-examples-svm-plot-rbf-parameters-py) are also recommended.
+
+#### Quick start
+
+You need to have [python](https://www.python.org/) installed (which includes package manager pip).  OSX supports multiple versions of python, but we will need python 3.7 or newer.  If you use brew to install python3, then python3 is an alias you can use.  To install python and scikit-learn on OSX try:
+
+     brew install python3
+     pip3 install scikit-learn xgboost==0.82 mlflow
+
+If you want a specific version of scikit the command would be pip3 install scikit-learn==0.20.1 (latest stable).  To play with [xgboost](https://xgboost.readthedocs.io/en/latest/python/python_api.html#module-xgboost.sklearn) you need to 'pip3 install xgboost'.
+
+#### Training and saving a model
+To train a classifier you would run:
+
+     python3 py/train.py
+
+You can run the the python scripts from IntelliJ with the python plug-in.
+
+#### Data
+
+There are 2660 pairs in csv format in the data directory 1193 neg (start with "0"), and 1467 pos (start with "1").  The fields then alternate after the label for target, then candidate.  They include person name, spouse, parent1, parent2, child, person birth year, month, day, country, county, city, location, census year, month, day, country, county, city, location, and death year, month, day, country, county, city, location.  The last field is a reference id.
---
+++ py/train.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+import sys
+import mlflow
+from mlflow import sklearn
+from time import time
+sys.path.append("./")
+from prepare import preprocess
+from sklearn.metrics import accuracy_score, recall_score, precision_score, average_precision_score, precision_recall_curve
+from xgboost import XGBClassifier, plot_tree, plot_importance
+import matplotlib.pyplot as plt
+
+X_train, X_test, y_train, y_test = preprocess()
+
+mlflow.set_experiment("matching")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+with mlflow.start_run():
+
+  params = {'n_estimators': 700, 
+            'eta': 0.1, 
+            'max_depth': 7, 
+            'gamma': 0.95, 
+            'subsample': 0.95, 
+            'silent': 1,
+            'objective': 'binary:logistic'}
+
+  for key in params:
+    mlflow.log_param(key, params[key])
+
+  earlyStopping = 10
+  mlflow.log_param("early_stopping_rounds", earlyStopping)
+
+  clf = XGBClassifier(**params)
+
+  t0 = time()
+  eval_set = [(X_train, y_train),(X_test, y_test)]
+  clf.fit(X_train, y_train, early_stopping_rounds=earlyStopping, eval_metric=["error", "logloss"], eval_set=eval_set, verbose=True)
+  print("training time:", round(time()-t0, 2), "s")
+
+  # performance metrics
+  results = clf.evals_result()
+  epochs = len(results['validation_0']['error'])
+  x_axis = range(0, epochs)
+
+  # plot log loss
+  fig, ax = plt.subplots()
+  ax.plot(x_axis, results['validation_0']['logloss'], label='Train')
+  ax.plot(x_axis, results['validation_1']['logloss'], label='Test')
+  ax.legend()
+  plt.ylabel('Log Loss')
+  plt.title('XGBoost Log Loss')
+  plt.savefig("Logloss.png")
+  mlflow.log_artifact("Logloss.png")
+  plt.show()
+
+  # plot classification error
+  fig, ax = plt.subplots()
+  ax.plot(x_axis, results['validation_0']['error'], label='Train')
+  ax.plot(x_axis, results['validation_1']['error'], label='Test')
+  ax.legend()
+  plt.ylabel('Classification Error')
+  plt.title('XGBoost Classification Error')
+  plt.savefig("Error.png")
+  mlflow.log_artifact("Error.png")
+  plt.show()
+
+
+  # persist the model
+  # clf.save_model('xgb.model')
+  # clf.dump_model('../models/xgb.raw.txt', 'featmap.txt')
+  mlflow.sklearn.log_model(clf, "model")
+
+  t0 = time()
+  pred = clf.predict(X_test)
+  print("prediction time:", round(time()-t0, 2), "s")
+
+  #####################################
+  # Evaluate predictions
+  # ...................................
+  accuracy = accuracy_score(y_test, pred)
+  print("Accuracy: %.2f%%" % (accuracy * 100.0))
+
+  mlflow.log_metric("accuracy", accuracy)
+
+  precision = precision_score(y_test, pred)
+  print("Precision: %.2f%%" % (precision * 100.0))
+
+  recall = recall_score(y_test, pred)
+  print("Recall: %.2f%%" % (recall * 100.0))
+
+  y_score = clf.predict_proba(X_test)[:,1]
+  average_precision = average_precision_score(y_test, y_score)
+  print("Average precision-recall score: %.2f%%" % (average_precision * 100.0))
+
+  importances = clf.get_booster().get_fscore()
+  for key in importances:
+    mlflow.log_metric(key, importances[key])
+
+  ##################################
+  # Plot the Precision-Recall curve
+  # ................................
+  plot_tree(clf)
+  plt.savefig("tree.png", dpi = 2400)
+  mlflow.log_artifact("tree.png")
+
+  precision, recall, _ = precision_recall_curve(y_test, y_score)
+  plt.figure(num=None, figsize=(8,6), dpi=160)
+  plt.step(recall, precision, color='b', alpha=0.2, where='post')
+
+  plt.xlabel('Recall')
+  plt.ylabel('Precision')
+  plt.title('Precision-Recall curve: AP={0:0.4f}'.format(average_precision))
+  plt.savefig("PR_curve.png")
+  mlflow.log_artifact("PR_curve.png")
+
+  plt.show()
