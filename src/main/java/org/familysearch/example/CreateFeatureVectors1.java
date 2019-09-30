1fd30312623505b8ef3466f55615b51efcc3bccd
Benjamin Hafen <benhafen@ldschurch.org>
2019-09-30 12:48:14 -0600

    Convert to LibSVM within CreateFeatureVectors class to be consistent with Python example

--- src/main/java/org/familysearch/example/CreateFeatureVectors1.java
+++ src/main/java/org/familysearch/example/CreateFeatureVectors1.java
@@ -1,19 +1,20 @@
 package org.familysearch.example;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class CreateFeatureVectors1 {
 
   private static final String OUTPUT_PATH = "data/javaVector";
+  private static final String VECTOR_FILE = "data/javaVector";
+  private static final String LIBSVM_TRAIN_FILE = "data/javaVector_train.libsvm";
+  private static final String LIBSVM_EVAL_FILE = "data/javaVector_eval.libsvm";
 
   public static void main(String[] args) {
     new CreateFeatureVectors1().run();
@@ -47,6 +48,7 @@ public class CreateFeatureVectors1 {
           Collections.singletonList(vectorValues.toString().replaceAll("([,\\[\\]])", "")),
           StandardCharsets.UTF_8,
           Files.exists(path) ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
+        createLibSvmFile();
       }
       catch (IOException e) {
         System.err.println("Unable to write to file " + OUTPUT_PATH);
@@ -54,6 +56,45 @@ public class CreateFeatureVectors1 {
       }
     }
 
+  }
+
+  private void createLibSvmFile() {
+    List<String> trainLines = new ArrayList<>();
+    List<String> evalLines = new ArrayList<>();
+    List<String> inputFile = Utils.readLines(VECTOR_FILE);
+    for (String line : inputFile) {
+      if (Math.random() > 0.15) {
+        trainLines.add(line);
+      }
+      else {
+        evalLines.add(line);
+      }
+    }
+    writeLibSvmLines(trainLines, LIBSVM_TRAIN_FILE);
+    writeLibSvmLines(evalLines, LIBSVM_EVAL_FILE);
+  }
+
+  private void writeLibSvmLines(List<String> inputFile, String libsvmEvalFile) {
+    Path libSvmEvalFile = Paths.get(libsvmEvalFile);
+    try (BufferedWriter writer = Files.newBufferedWriter(libSvmEvalFile)) {
+      StringJoiner sj = new StringJoiner(" ");
+      for (String line : inputFile) {
+        String[] features = line.split(" ");
+        sj.add(features[0]);
+        for (int i = 1; i < features.length; i++) {
+          String featureScore = features[i];
+          if (!"0".equals(featureScore)) {
+            sj.add(i + ":" + featureScore);
+          }
+        }
+        sj.add("\n");
+      }
+      writer.write(sj.toString());
+    }
+    catch (IOException e) {
+      System.err.println("Unable to write to file " + libsvmEvalFile);
+      e.printStackTrace();
+    }
   }
 }
 
--- src/main/java/org/familysearch/example/Train.java
+++ src/main/java/org/familysearch/example/Train.java
@@ -5,16 +5,11 @@ import ml.dmlc.xgboost4j.java.DMatrix;
 import ml.dmlc.xgboost4j.java.XGBoost;
 import ml.dmlc.xgboost4j.java.XGBoostError;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Train {
 
-  private static final String VECTOR_FILE = "data/javaVector";
   private static final String LIBSVM_TRAIN_FILE = "data/javaVector_train.libsvm";
   private static final String LIBSVM_EVAL_FILE = "data/javaVector_eval.libsvm";
   private static final String OUTPUT_MODEL_FILE = "xgboost.model";
@@ -31,7 +26,6 @@ public class Train {
   }
 
   public void run() throws XGBoostError {
-    createLibSvmFile();
     DMatrix trainMatrix = new DMatrix(LIBSVM_TRAIN_FILE);
     DMatrix evalMatrix = new DMatrix(LIBSVM_EVAL_FILE);
 
@@ -52,44 +46,4 @@ public class Train {
     Booster booster = XGBoost.train(trainMatrix, params, TRAINING_ITERATIONS, watches, null, null);
     booster.saveModel(OUTPUT_MODEL_FILE);
   }
-
-  private void createLibSvmFile() {
-    List<String> trainLines = new ArrayList<>();
-    List<String> evalLines = new ArrayList<>();
-    List<String> inputFile = Utils.readLines(VECTOR_FILE);
-    for (String line : inputFile) {
-      if (Math.random() > 0.15) {
-        trainLines.add(line);
-      }
-      else {
-        evalLines.add(line);
-      }
-    }
-    writeLibSvmLines(trainLines, LIBSVM_TRAIN_FILE);
-    writeLibSvmLines(evalLines, LIBSVM_EVAL_FILE);
-  }
-
-  private void writeLibSvmLines(List<String> inputFile, String libsvmEvalFile) {
-    Path libSvmEvalFile = Paths.get(libsvmEvalFile);
-    try (BufferedWriter writer = Files.newBufferedWriter(libSvmEvalFile)) {
-      StringJoiner sj = new StringJoiner(" ");
-      for (String line : inputFile) {
-        String[] features = line.split(" ");
-        sj.add(features[0]);
-        for (int i=1; i<features.length; i++) {
-          String featureScore = features[i];
-          if (!"0".equals(featureScore)) {
-            sj.add(i + ":" + featureScore);
-          }
-        }
-        sj.add("\n");
-      }
-      writer.write(sj.toString());
-    }
-    catch (IOException e) {
-      System.err.println("Unable to write to file " + libsvmEvalFile);
-      e.printStackTrace();
-    }
-  }
-
 }
